4649fb10bb1dea9bf8c2749b5a30b9ee2247f2ed
ZacLou <zac.lou.bounty@gmail.com>
2026-08-31 19:15:45 +0800

    test(payments): reject empty memo strings in create_intent (#34)

--- contracts/payments/src/test.rs
+++ contracts/payments/src/test.rs
@@ -81,3 +81,19 @@ fn rejects_settle_after_cancellation() {
     client.cancel_intent(&id);
     client.settle_intent(&id, &soroban_string(&env, "tx-0002"));
 }
+
+#[test]
+#[should_panic]
+fn rejects_empty_memo_in_create_intent() {
+    let env = test_env();
+    let admin = test_address(&env);
+    let treasury = test_address(&env);
+    let payer = test_address(&env);
+    let payee = test_address(&env);
+
+    let contract_id = env.register(PaymentsContract, ());
+    let client = PaymentsContractClient::new(&env, &contract_id);
+
+    client.initialize(&admin, &treasury, &50_u32);
+    client.create_intent(&payer, &payee, &5_000_i128, &soroban_string(&env, ""));
+}
